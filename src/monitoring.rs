252fcc0ca00e81326207d0b7a056b972cb5e70fd
Maxim Gorin <maksim.g@simple.app>
2023-02-06 19:13:53 +0300

    Refactoring

--- src/monitoring.rs
+++ src/monitoring.rs
@@ -3,39 +3,39 @@ use crate::{
     caches::PositionsCache,
     positions::{BidAsk, ClosedPosition, Position},
 };
-use tokio::sync::RwLock;
 
 pub struct PositionsMonitor {
-    positions_cache: RwLock<PositionsCache>,
+    positions_cache: PositionsCache,
 }
 
 impl PositionsMonitor {
     pub fn new(positions_cache: PositionsCache) -> Self {
         Self {
-            positions_cache: RwLock::new(positions_cache),
+            positions_cache,
         }
     }
 
-    pub async fn add_position(&mut self, position: Position) {
-        let mut cache = self.positions_cache.write().await;
-        cache.add(position);
+    pub fn remove_position(&mut self, position_id: &str, wallet_id: &str) -> Option<Position> {
+        return self.positions_cache.remove(position_id, wallet_id);
     }
 
-    pub async fn get_positions(&mut self, wallet_id: &str) -> Vec<Rc<Position>> {
-        let cache = self.positions_cache.read().await;
-        let positions = cache.get_by_wallet_id(wallet_id);
+    pub fn add_position(&mut self, position: Position) {
+        self.positions_cache.add(position);
+    }
+
+    pub fn get_positions(&mut self, wallet_id: &str) -> Vec<Rc<Position>> {
+        let positions = self.positions_cache.get_by_wallet_id(wallet_id);
 
         positions
     }
 
-    pub async fn update_positions(&self, bidask: &BidAsk) -> Vec<ClosedPosition> {
+    pub fn update_positions(&mut self, bidask: &BidAsk) -> Vec<ClosedPosition> {
         let mut closed_positions = Vec::new();
-        let mut cache = self.positions_cache.write().await;
-        let positions = cache.get_by_instrument(&bidask.instrument);
+        let positions = self.positions_cache.get_by_instrument(&bidask.instrument);
 
         for position in positions {
             let position =
-                cache.remove(&position.get_id(), &position.get_order().wallet_id);
+            self.positions_cache.remove(&position.get_id(), &position.get_order().wallet_id);
             let position = position.unwrap();
 
             match position {
@@ -45,7 +45,7 @@ impl PositionsMonitor {
                 Position::Pending(mut pending_position) => {
                     pending_position.update(bidask);
                     let position = pending_position.try_activate();
-                    cache.add(position);
+                    self.positions_cache.add(position);
                 }
                 Position::Active(mut active_position) => {
                     active_position.update(bidask);
@@ -53,10 +53,10 @@ impl PositionsMonitor {
                     match position {
                         Position::Closed(closed_position) => closed_positions.push(closed_position),
                         Position::Active(position) => {
-                            cache.add(Position::Active(position))
+                            self.positions_cache.add(Position::Active(position))
                         }
                         Position::Pending(position) => {
-                            cache.add(Position::Pending(position))
+                            self.positions_cache.add(Position::Pending(position))
                         }
                     }
                 }
